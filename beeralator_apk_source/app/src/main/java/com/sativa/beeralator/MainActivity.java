8fef85bb69ab469df7ed624786b4e318d97dafe1
jacktenor <jacktenor1@gmail.com>
2020-09-25 09:59:17 -0400

    no button

--- beeralator_apk_source/app/src/main/java/com/sativa/beeralator/MainActivity.java
+++ beeralator_apk_source/app/src/main/java/com/sativa/beeralator/MainActivity.java
@@ -1,19 +1,12 @@
 package com.sativa.beeralator;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Objects;
-
-import static android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,52 +29,43 @@ public class MainActivity extends AppCompatActivity {
         input3 = findViewById(R.id.et_input3);
         input4 = findViewById(R.id.et_input4);
 
-        Button bt_calculate = findViewById(R.id.bt_calculate);
-
+        EditText edit_txt = findViewById(R.id.et_input4);
         tv_result = findViewById(R.id.tv_result);
         tv_result2 = findViewById(R.id.tv_result2);
 
-        bt_calculate.setOnClickListener(new OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        edit_txt.setOnEditorActionListener(new EditText.OnEditorActionListener() {
             @Override
-            public void onClick(View view) {
-                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(), 0);
-                makeCalculations();
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    input4.performClick();
+
+                    double doorfee = Double.parseDouble(input5.getText().toString());
+                    double people = Double.parseDouble(input1.getText().toString());
+                    double beersperhour = Double.parseDouble(input2.getText().toString());
+                    double hours = Double.parseDouble(input3.getText().toString());
+                    double contributions = doorfee * people;
+                    double cost = Double.parseDouble(input4.getText().toString());
+                    double result = (int) Math.ceil(people * beersperhour * hours / 24);
+                    double result2 = (result * cost) - contributions;
 
 
+                    String sValue = String.format("%,.0f", result);
+                    String sValue2 = String.format("%,.2f", (Math.abs(result2)));
+
+                    tv_result.setText(String.format(" Required cases = %s", sValue));
+
+                    if (result2 >= 0) {
+                        tv_result2.setText(String.format(" Cost = $%s", sValue2));
+                    } else {
+                        tv_result2.setText(String.format(" Profit = $%s", sValue2));
+
+                    }
+
+                }
+
+                return false;
             }
+
         });
     }
-
-
-
-    private void makeCalculations() {
-        double doorfee = Double.parseDouble(input5.getText().toString());
-        double people = Double.parseDouble(input1.getText().toString());
-        double beersperhour = Double.parseDouble(input2.getText().toString());
-        double hours = Double.parseDouble(input3.getText().toString());
-        double contributions = doorfee * people;
-        double cost = Double.parseDouble(input4.getText().toString());
-        double result = (int) Math.ceil(people * beersperhour * hours / 24);
-        double result2 = (result * cost) - contributions;
-
-
-        String sValue = String.format("%,.0f", result);
-        String sValue2 = String.format("%,.2f", (Math.abs(result2)));
-
-        tv_result.setText(String.format(" Required cases = %s", sValue));
-
-        if (result2 >= 0) {
-        tv_result2.setText(String.format(" Cost = $%s", sValue2));
-    }
-        else {
-            tv_result2.setText(String.format(" Profit = $%s", sValue2));
-
-        }
-
-    }
-
-
-
 }
