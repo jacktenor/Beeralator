7343900c43b0853817b9bd4628735e83a29b4d61
jacktenor <jacktenor1@gmail.com>
2020-09-25 23:58:55 -0400

    FREE BEER!!!

--- beeralator_apk_source/app/src/main/java/com/sativa/beeralator/MainActivity.java
+++ beeralator_apk_source/app/src/main/java/com/sativa/beeralator/MainActivity.java
@@ -11,12 +11,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private EditText input5;
+    private TextView tv_textView;
+    private TextView tv_textView1;
+    private TextView tv_textView2;
+    private TextView tv_textView3;
+    private TextView tv_textView4;
     private EditText input1;
     private EditText input2;
     private EditText input3;
     private EditText input4;
     private TextView tv_result;
-    private TextView tv_result2;
+    private TextView tv_result2;;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
         EditText edit_txt = findViewById(R.id.et_input4);
         tv_result = findViewById(R.id.tv_result);
         tv_result2 = findViewById(R.id.tv_result2);
+        tv_textView = findViewById(R.id.tv_textView);
+        tv_textView1 = findViewById(R.id.tv_textView1);
+        tv_textView2 = findViewById(R.id.tv_textView2);
+        tv_textView3 = findViewById(R.id.tv_textView3);
+        tv_textView4 = findViewById(R.id.tv_textView4);
+
 
         edit_txt.setOnEditorActionListener(new EditText.OnEditorActionListener() {
             @Override
@@ -47,25 +59,40 @@ public class MainActivity extends AppCompatActivity {
                     double cost = Double.parseDouble(input4.getText().toString());
                     double result = (int) Math.ceil(people * beersperhour * hours / 24);
                     double result2 = (result * cost) - contributions;
+                    if (doorfee == 0) {
+                        tv_textView.setText(" FREE BEER!!!");
+                    }
+                    if (people >= 1000) {
+                        tv_textView1.setText(" Call in the National Gaurd!!!");
+                    }
+                    if (beersperhour >= 6) {
+                        tv_textView2.setText(" Bring on the coma!");
+                    }
+                    if (hours >= 7) {
+                        tv_textView3.setText(" A good old fashion all nighter!");
+                    }
+                    if (cost >= 30) {
+                        tv_textView4.setText(" Drinking the good stuff.");
+                    }
+                        String sValue = String.format("%,.0f", result);
+                        String sValue2 = String.format("%,.2f", (Math.abs(result2)));
 
+                        tv_result.setText(String.format(" Required cases = %s", sValue));
 
-                    String sValue = String.format("%,.0f", result);
-                    String sValue2 = String.format("%,.2f", (Math.abs(result2)));
+                        if (result2 >= 0) {
+                            tv_result2.setText(String.format(" Cost = $%s", sValue2));
+                        } else {
+                            tv_result2.setText(String.format(" Profit = $%s", sValue2));
 
-                    tv_result.setText(String.format(" Required cases = %s", sValue));
-
-                    if (result2 >= 0) {
-                        tv_result2.setText(String.format(" Cost = $%s", sValue2));
-                    } else {
-                        tv_result2.setText(String.format(" Profit = $%s", sValue2));
+                        }
 
                     }
 
+                    return false;
                 }
 
-                return false;
-            }
 
         });
     }
 }
+
